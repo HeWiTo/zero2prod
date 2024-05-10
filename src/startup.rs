5ab6383d83b8f1aabc9464abfefbb7d4c321e217
Henri <h.w.tompodung@gmail.com>
2024-05-10 15:17:23 +0700

    Logging - POST /subscriptions

--- src/startup.rs
+++ src/startup.rs
@@ -4,6 +4,8 @@ use crate::routes::{
 };
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::middleware::Logger;
 use std::net::TcpListener;
 use sqlx::PgPool;
 
@@ -11,9 +13,10 @@ pub fn run(
     listener: TcpListener,
     db_pool: PgPool
 ) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move|| {
             App::new()
+                .wrap(Logger::default())
                 .route("/health_check", web::get().to(health_check))
                 .route("/subscriptions", web::post().to(subscribe))
                 .app_data(db_pool.clone())
